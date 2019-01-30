cb6f7a10eb2a7a11bebfda5e8734ae814ac15ed9
Žan Gaberc <37248532+zangaberc@users.noreply.github.com>
2019-01-30 23:30:24 +0100

    Missing
    node_modules

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+	<p>
+	<input type="checkbox" v-on:change="markComplete"> 
+	{{todo.title}}</p>
+	<button @click="$emit('del-todo', todo.id)" class="del">x</button>
+</div>
+</template>
+
+<script>
+export default {
+	name: "TodoItem",
+	props: ["todo"],
+	methods: {
+		markComplete () {
+			this.todo.completed = !this.todo.completed;
+		}
+	}
+}
+</script>
+
+<style scoped>
+.todo-item{
+	background: #f4f4f4;
+	padding: 20px;
+	border-bottom: 1px #ccc dotted;
+}
+.is-complete{
+	text-decoration: line-through;
+}
+.del{
+	background: #ff0000;
+	color: #fff;
+	border: none;
+	padding: 3px 10px;
+	border-radius: 10%;
+	cursor: pointer;
+	float: right;
+}
+
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<header class="header">
+	<h1>TodoList</h1>
+	<div id="nav">
+		<router-link to="/"> Home </router-link>
+		<router-link to="about"> About </router-link>			
+	</div>
+</header>
+</template>
+
+<script>
+export default {
+	name: "Header"
+}
+</script>
+
+<style scoped>
+.header {
+	background: #333;
+	color: red;
+	text-align: center;
+	padding: 10px;
+}
+.header a {
+	color: red;
+	padding-right: 5px;
+	text-decoration: none;
+}
+</style>
